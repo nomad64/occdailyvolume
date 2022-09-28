f82a7409474427294a55c1cac241a551e8c78ad6
Jon Sharpe <nomad64@gmail.com>
2022-09-28 13:39:16 -0500

    cleanup

--- occ-daily-volume/common/logging.py
+++ occ-daily-volume/common/logging.py
@@ -6,12 +6,11 @@ from datetime import datetime
 
 def setup_logging(name_):
     """
-    Setup simple console and file logging for the script to use
+    Setup simple console and file logging
 
     :param name_: name of app using logging, ideally should be __name__
     :return:
     """
-    # log_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log')
     log_file_name = f"{name_}_{datetime.now().strftime('%m-%d-%Y_%H%M%S')}.log"
     CONFIG = {'version': 1,
               'disable_existing_loggers': True,
