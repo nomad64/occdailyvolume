20d6a0487dabfd940438e677059ffc89d241e722
Jon Sharpe <nomad64@gmail.com>
2022-10-01 08:22:16 -0500

    docstrings

--- occ-daily-volume/common/sqlite.py
+++ occ-daily-volume/common/sqlite.py
@@ -1,6 +1,17 @@
 import sqlite3 as sql
 
 def db_write_df_to_sql(db_filepath: str, db_table: str, df_: pd.DataFrame):
+    """
+    Write given dataframe to SQLite DB file
+
+    :param db_filepath: database filepath
+    :type db_filepath: str
+    :param db_table: database table to write
+    :type db_table: str
+    :param df_: dataframe to write
+    :type df_: pd.DataFrame
+    """
+    logger = logging.getLogger(__name__)
     conn = sql.connect(db_filepath)
     df_len = len(df_)
     logger.debug(f"Starting write of {df_len:,} rows to {db_filepath}")
@@ -9,7 +20,16 @@ def db_write_df_to_sql(db_filepath: str, db_table: str, df_: pd.DataFrame):
     conn.close()
 
 
-def db_read_sql_to_df(db_filepath: str, db_table: str):
+def db_read_sql_to_df(db_filepath: str, db_table: str) -> pd.DataFrame:
+    """
+    Read given DB file into dataframe
+
+    :param db_filepath: database filepath
+    :type db_filepath: str
+    :param db_table: database table to read
+    :type db_table: str
+    """
+    logger = logging.getLogger(__name__)
     conn = sql.connect(db_filepath)
     logger.debug(f"")
     pd.read_sql_query("SELECT * from volHist", conn)
